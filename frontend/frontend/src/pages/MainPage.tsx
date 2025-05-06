97a21647fad042b83967106a96f6918ff3343e02
Danil-Nazarenko <danil.nazarenko2109@gmail.com>
2025-05-06 21:11:38 +0300

    Небольшая работа с MainPage

--- frontend/frontend/src/pages/MainPage.tsx
+++ frontend/frontend/src/pages/MainPage.tsx
@@ -1,36 +1,43 @@
-import { Box, Typography, Paper } from '@mui/material';
+import { Box, Drawer, List, ListItemText, ListItemButton, Typography } from '@mui/material';
 
 const MainPage = () => {
   return (
-    <Box 
-      sx={{
-        minHeight: '100vh',
-        bgcolor: '#1e1e1e',
-        color: '#fff',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        paddingTop: 8,
-      }}
-    >
-      <Paper 
-        elevation={4}
+    <Box sx={{ display: 'flex', height: '100vh', bgcolor: '#14353b' }}>
+
+      <Drawer
+        variant="permanent"
+        anchor="left"
         sx={{
-          p: 4,
-          width: '90%',
-          maxWidth: 800,
-          borderRadius: 3,
-          backgroundColor: '#2c2c2c',
+          width: 200,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: 200,
+            boxSizing: 'border-box',
+            bgcolor: '#1e545e',
+            color: '#fff',
+          },
         }}
       >
-        <Typography variant="h4" gutterBottom fontWeight="bold">
-          Панель задач
+        <Typography variant="h6" textAlign="center" sx={{ mt: 1 }}>
+          LocOp
         </Typography>
-        <Typography variant="body1">
-          Добро пожаловать! Здесь будет отображаться список ваших задач.
+        <List>
+          {['Главная', 'Задачи', 'Профиль'].map((text) => (
+          <ListItemButton key={text}>
+           <ListItemText primary={text} />
+           </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Typography variant="h4" color="#fff">
+          Добро пожаловать!
         </Typography>
-        {/* Позже здесь будет: список задач, фильтры, кнопки и т.д. */}
-      </Paper>
+        <Typography variant="body1" color="#ccc">
+          Это ваша главная страница.
+        </Typography>
+      </Box>
     </Box>
   );
 };
--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8723a87525eecdd55514c5d2a398fc6614cd22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16c76c057eb2741558739269a6eddb0be310885")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
